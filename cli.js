f1c07b7aad4f391638cf2cb2183b16b478036d01
yuki <y09425717@gmail.com>
2026-09-08 16:44:10 +0900

    入力値のバリデーションを実装

--- cli.js
+++ cli.js
@@ -3,6 +3,12 @@ import enquirer from "enquirer";
 export const askTotal = async () => {
   const prompt = new enquirer.Input({
     message: "総支給額を入力してください",
+    validate: (value) => {
+      const result = validatePositiveNumber(value);
+      if (result !== true) return result;
+      if (!Number.isInteger(Number(value))) return "整数を入力してください。";
+      return true;
+    },
   });
   const answer = await prompt.run();
   return Number(answer);
@@ -11,6 +17,7 @@ export const askTotal = async () => {
 export const askMonthlyHours = async () => {
   const prompt = new enquirer.Input({
     message: "所定労働時間を入力してください",
+    validate: validatePositiveNumber,
   });
   const answer = await prompt.run();
   return Number(answer);
@@ -19,7 +26,15 @@ export const askMonthlyHours = async () => {
 export const askOvertimeHours = async () => {
   const prompt = new enquirer.Input({
     message: "みなし残業時間を入力してください",
+    validate: validatePositiveNumber,
   });
   const answer = await prompt.run();
   return Number(answer);
+};
+
+const validatePositiveNumber = (value) => {
+  const number = Number(value);
+  if (Number.isNaN(number)) return "数値を入力してください。";
+  if (number <= 0) return "0より大きい金額を入力してください。";
+  return true;
 };
